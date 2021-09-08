6ccaecfb485d41837724742b0c56f05a95e97c20
Todd Pataky <todd@MacBook-Pro-16-todo.local>
2021-09-08 12:11:29 +0900

    Update Lessons 1 & 2

---
+++ Snippets/exporrt_notebooks.py
@@ -0,0 +1,28 @@
+
+import os,pathlib
+
+dirREPO      = pathlib.Path( __file__ ).parent.parent
+
+
+def export_ipynb_as_html_embed(fpathIPYNB):
+	cmd          = f'jupyter nbconvert --template classic --to html_embed {fpathIPYNB}'
+	os.system( cmd )
+
+
+# # export single notebook:
+# fpathIPYNB   = os.path.join(dirREPO, 'Lessons', 'Lesson01', '1-IntroductionToJupyter', 'IntroductionToJupyter.ipynb')
+# export_ipynb_as_html_embed( fpathIPYNB )
+
+
+# export all notebooks for one lesson:
+lesson       = 2
+dirLESSON    = os.path.join( dirREPO, 'Lessons', f'Lesson{lesson:02}')
+for root,dname,fnames in os.walk(dirLESSON):
+	for f in fnames:
+		if f.endswith('.ipynb') and not ('checkpoint' in f):
+			fpathIPYNB = os.path.join(root, f)
+			export_ipynb_as_html_embed( fpathIPYNB )
+
+
+
+
